34c26aa4fcf263ef489604a0adea54e3311a82c1
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2014-04-15 23:14:42 +0900

    最小値を求めることができるキュー

--- find_min_stack_queue.py
+++ find_min_stack_queue.py
@@ -30,12 +30,38 @@ class Stack:
     def findmin(self):
         return self._min
 
-class Stack:
-    pass
+class Queue:
+    def __init__(self):
+        self._que = []
+        self._min_que = []
+
+    def enqueue(self, i):
+        idx = len(self._min_que)
+        for j in reversed( range( len(self._min_que) ) ):
+            idx = j if i < self._min_que[j] else idx
+        self._min_que = self._min_que[:idx]
+
+        self._que.append(i)
+        self._min_que.append(i)
+
+    def dequeue(self):
+        if not( len(self._que) > 0 ):
+            return None
+
+        ret = self._que[0]
+        self._que = self._que[1:]
+        if ret == self._min_que[0]:
+            self._min_que = self._min_que[1:]
+
+        return ret
+
+    def findmin(self):
+        return None if not( len(self._min_que) > 0 ) else self._min_que[0]
 
 if __name__ == "__main__":
     #random.seed(1)
     s = Stack()
+    print("------------------------------")
     print("s.findmin()", s.findmin())
     print("s.pop:", s.pop())
     randint = range(10)
@@ -46,3 +72,19 @@ if __name__ == "__main__":
         print("s.findmin()", s.findmin())
         if random.randint(0,1):
             print("s.pop:", s.pop())
+        print("")
+
+    #random.seed(1)
+    q = Queue()
+    print("------------------------------")
+    print("q.findmin()", q.findmin())
+    print("q.dequeue():", q.dequeue())
+    randint = range(10)
+    random.shuffle(randint)
+    for i in randint:
+        q.enqueue(i)
+        print("q.enqueue(%d)" % i, q._que)
+        print("q.findmin()", q.findmin())
+        if random.randint(0,1):
+            print("q.dequeue():", q.dequeue())
+        print("")
