684a3874af80f94e8d45b6f9ce0cfd3679732a45
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2014-04-20 22:14:27 +0900

    パターンマッチング; BoyerMoore法を実装

--- pattern_matching.py
+++ pattern_matching.py
@@ -51,15 +51,45 @@ def KnuthMorrisPratt(T, P):
                 i = Q[i]
     return pos
 
+def BoyerMoore(T, P):
+    N, M = len(T), len(P)
+    # construct shift table
+    table = {}
+    for i,c in enumerate( reversed(P) ):
+        if not table.has_key(c):
+            table[c] = i
+    table[ P[-1] ] = M
+
+    # serach
+    pos = []
+    i = M - 1
+    while i < N:
+        j = 0
+        while j < M:
+            if T[i-j] != P[(M-1)-j]: break
+            j += 1
+        if j == M:
+            pos.append(i-j+1)
+            i += M
+        else:
+            i += M if not table.has_key(T[i-j]) else table[ T[i-j] ]
+
+    return pos
+
 if __name__ == '__main__':
     T = "this it the sample text from which Search methods search the Pattern."
     T += "Naive Search Algorithm needs O(NM) comparison."
     T += "KnuthMorrisPratt Search Algorithm can search only with O(N+M) comparison."
+    T += "BoyerMoore Search Algorithm also can search only with O(N+M) comparison."
     P = "Search"
+
     print("Naive(T,P): ", Naive(T,P))
     print("KnuthMorrisPratt(T,P): ", KnuthMorrisPratt(T,P))
+    print("BoyerMoore(T,P): ", BoyerMoore(T,P))
 
     t1 = timeit.Timer('Naive(T,P)', 'from __main__ import T, P, Naive')
     t2 = timeit.Timer('KnuthMorrisPratt(T,P)', 'from __main__ import T, P, KnuthMorrisPratt')
+    t3 = timeit.Timer('BoyerMoore(T,P)', 'from __main__ import T, P, BoyerMoore')
     print("Naive: ", t1.timeit(10000))
     print("KnuthMorrisPratt: ", t2.timeit(10000))
+    print("BoyerMoore: ", t3.timeit(10000))
