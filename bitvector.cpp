a0e05c05896f499d9779ac7aca568a57110a5f05
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2014-10-22 23:34:15 +0200

    debug; enable to change the size of each block, S, L and P

--- bitvector.cpp
+++ bitvector.cpp
@@ -6,129 +6,143 @@
 #include<string.h>
 #include<math.h>
 
+#define TYPE_TO_BITS(X) (8 * sizeof(X))
+#define DIVIDE8(X) ((X) >> 3)
+#define MOD8(X) ((X) & 0x07)
+#define LSB(X) ((X) & 0x01)
+
 using namespace std;
 
-typedef unsigned char sBlock;
-typedef unsigned short lBlock;
+typedef unsigned char block_b; /* DO NOT CHANGE THE SIZE OF B[i] */
+typedef unsigned short block_s;
+typedef unsigned int block_l;
+typedef unsigned char block_p;
+typedef unsigned long long ULL;
+
+typedef size_t size_array;
+typedef size_t size_bits;
 
 class BitVector{
 private:
-    int n, l, s;
-    size_t sBLC, lBLC, len_l, len_s, len_p, size_l, size_s;
-    vector<sBlock> B, S, P;
-    vector<lBlock> L;
+    size_array len_S, len_L, len_P, len_B;
+    size_bits bits_B, bits_P, bits_S, bits_L, n, b, s, l;
+    vector<block_b> B; /* in which a bit-vector is stored */
+    vector<block_s> S; /* S: small blocks */
+    vector<block_l> L; /* L: large blocks */
+    vector<block_p> P; /* P: a dictionary of # of 1 in each bit-vector */
 public:
     ~BitVector(){
     };
     BitVector(const char* _B){
         //----- initialize constants -----
-        sBLC = 8 * sizeof(sBlock);
-        lBLC = 8 * sizeof(lBlock);
-        n = strlen(_B);
-        int log2n = (int)ceil( log2(n) );
-        /* s = lg(n)/2 bits are covered by S[i] */
-        /* S[i] can be stored in lg(l) (bits) */
-        s = ceil( log2n / 2. );
-        len_s = (int)ceil( n / (double)s );
-        /* l = lg^2(n) bits are covered by L[i] */
-        /* L[i] can be stored in lg(n) (bits) */
-        l = log2n * log2n;
-        while( l % s ){ l++; }
-        len_l = (int)ceil( n / (double)l );
-        /* |P[0, 2^s)| */
-        len_p = 1 << s;
+        bits_B = b = TYPE_TO_BITS(block_b);
+        bits_P = TYPE_TO_BITS(block_p);
+        bits_S = TYPE_TO_BITS(block_s);
+        bits_L = TYPE_TO_BITS(block_l);
 
-        size_l = (int)ceil( log2(n) );
-        size_s = (int)ceil( log2(l) );
-        if( size_l > lBLC ){
-            printf("exceed the expected number of bits for L[i], L[i] needs %d bits\n", (int)size_l);
+        n = (size_bits)strlen(_B);
+        int log2n = (int)ceil( log2( (int)n ) );
+        s = (size_bits)ceil( log2n / 2. ); /* s = lg(n)/2 bits in B are covered by S[i] */
+        l = (size_bits)(log2n * log2n); /* l = lg^2(n) bits in B are covered by L[i] */
+        while( l % s ){ l++; } /* make l become a multiple of s */
+
+        len_B = (size_array)ceil( (int)n / (double)b ); /* |B[]| */
+        len_S = (size_array)ceil( (int)n / (double)s ); /* |S[]| */
+        len_L = (size_array)ceil( (int)n / (double)l ); /* |L[]| */
+        len_P = (size_array)(1 << s); /* |P[0, 2^s)| */
+
+        //----- check if each specified size is enough -----
+        size_bits required_bits_p, required_bits_s, required_bits_l;
+        required_bits_p = (size_bits)ceil( log2((int)s) );
+        required_bits_s = (size_bits)ceil( log2((int)l) );
+        required_bits_l = (size_bits)ceil( log2((int)n) );
+        if( bits_P < required_bits_p ){
+            printf("exceed # of bits of P[i], P[i] needs %d bits\n", (int)required_bits_p );
             exit(1);
         }
-        if( size_s > sBLC ){
-            printf("exceed the expected number of bits for S[i], S[i] needs %d bits\n", (int)size_s);
+        if( bits_S < required_bits_s ){
+            printf("exceed # of bits of S[i], S[i] needs %d bits\n", (int)required_bits_s );
+            exit(1);
+        }
+        if( bits_L < required_bits_l ){
+            printf("exceed # of bits of L[i], L[i] needs %d bits\n", (int)required_bits_l );
             exit(1);
         }
 
-        printf("the size of a smaller block: %d\n", (int)sBLC);
-        printf("the size of a larger block: %d\n", (int)lBLC);
-        printf("n=%d\n", n);
-        printf("l=%d, sizeof(L[i])=%d(bits)\n", l, (int)size_l);
-        printf("s=%d, sizeof(S[i])=%d(bits)\n", s, (int)size_s);
-        printf("|P|=%d\n", (int)len_p);
+        //----- show status -----
+        printf("n: %d\n", (int)n);
+        printf("# of bits in B, that are covered by L[i]:%d\n", (int)l);
+        printf("# of bits in B, that are covered by S[i]:%d\n", (int)s);
+        printf("len(B): %d, # of bits of B[i]: %d\n", (int)len_B, (int)b);
+        printf("len(S): %d, # of bits of S[i]: %d (%d bits are required)\n", \
+                (int)len_S, (int)bits_S, (int)required_bits_s );
+        printf("len(L): %d, # of bits of L[i]: %d (%d bits are required)\n", \
+                (int)len_L, (int)bits_L, (int)required_bits_l );
+        printf("len(P): %d  # of bits of P[i]: %d (%d bits are required)\n", \
+                (int)len_P, (int)bits_P, (int)required_bits_p);
 
-        //----- reserve mamory spaces of B, L, S, and P -----
-        B.resize( n >> 3 );
-        L.resize( len_l );
-        S.resize( len_s );
-        P.resize( len_p );
+        //----- reserve mamories for B, L, S, and P -----
+        B.resize( len_B );
+        L.resize( len_L );
+        S.resize( len_S );
+        P.resize( len_P );
 
         //----- create bit vector -----
         L[0] = S[0] = 0;
-        for(int i=0,j=0,k=0; i < n; ++i){
-            if( !( i & 0x07 ) ){ B[ i >> 3 ] = 0; }
+        /* i: the index of _B ('i' is NOT the position of a bit),
+         * j: the index of L,
+         * k: the index of S */
+        for(int i=0, j=0, k=0; i < n; ++i){
+            if( !MOD8(i) ){ B[ DIVIDE8(i) ] = 0; } /* initialize each elem of B */
 
             if( !( i % l ) ){
                 ++j;
-                if( j < len_l ){ L[j] = L[j-1]; };
+                if( j < len_L ){ L[j] = L[j-1]; };
             }
 
             if( !( i % s ) ){
-                if( !( i % l ) ){ S[k] = 0; }
+                if( !( i % l ) ){ S[k] = 0; } /* if the index of L, j is also incremented */
                 ++k;
-                if( k < len_s ){ S[k] = S[k-1]; }
+                if( k < len_S ){ S[k] = S[k-1]; }
             }
 
             if( _B[i] - '0' ){
-                B[ i >> 3 ] |= 1 << (i & 0x07);
-                if( j < len_l ){ ++L[j]; }
-                if( k < len_s ){ ++S[k]; }
+                /* |-B[i+1]-|-B[ i ]-|
+                 * |87654321|87654321|*/
+                B[ DIVIDE8(i) ] |= 1 << MOD8(i); /* regist bits from right to left */
+                if( j < len_L ){ ++L[j]; }
+                if( k < len_S ){ ++S[k]; }
             }
 
         }
 
         //----- initialize P[0,i) -----
-        for(int i=0; i < (1 << s); ++i){
+        for(int i=0; i < len_P; ++i){
             P[i] = 0;
-            for(int j=1; j <= (1 << s); j <<= 1){
-                if( i & j ){ ++P[i]; }
+            for(int j=i; j > 0; j >>= 1){ /* check bits from right to left */
+                if( LSB(j) ){ ++P[i]; }
             }
         }
 
     };
 
     const char access(int i){
-        return (B[ i >> 3 ] & ( 1 << ( i & 0x07 ) )) ? '1' : '0';
+        return (B[ DIVIDE8(i) ] & ( 1 << MOD8(i) )) ? '1' : '0';
     };
 
     int rank(int i){
-        // (1) extract bits covered by S[i] but not B[i]
-        sBlock bitseq = 0;
+        // (1) extract bits in B[i], that are covered by S[i]
+        ULL bitseq = 0;
         int st = s * (i / s), en = st + s;
-        //// if 'st' and 'en' are stored in the same B[i]
-        if( (st >> 3) == (en >> 3) ){
-            for(int j=0; j<s; ++j){
-                if( B[ st >> 3 ] & ( 1 << (j + (st & 0x07) ) ) ){ bitseq |= (1 << j); };
-            }
-        //// if 'st' and 'en' are stored in separate sBlocks
-        }else{
-            for(int j=0; j<sBLC-(st & 0x07); ++j){
-                if( B[ st >> 3 ] & ( 1 << (j + (st & 0x07) ) ) ){ bitseq |= (1 << j); };
-            }
-            for(int j=0; j<(en & 0x07); ++j){
-                if( B[ en >> 3 ] & ( 1 << j ) ){ bitseq |= (1 << ( j + ( sBLC - (st & 0x07) ) )); };
-            }
-
+        for(int j=0; j<s; j++){
+            if( B[ DIVIDE8(j + st) ] & ( 1 << MOD8(j + st) ) ){ bitseq |= (1 << j); }
         }
 
         // (2) create a mask with the same width of the bits
-        //// the following line means -> sBlock mask = 0xff >> ( (BLC - s) + (s - i % s) );
-        sBlock mask = 0xff >> (sBLC - i % s);
+        ULL mask = 0;
+        for(int j=(i % s)-1; j >= 0; j--){ mask |= (1 << j); }
 
-//        cout << "L[" << i/l << "]: " << (int)L[ i / l ];
-//        cout << ", S[" << i/s << "]: " << (int)S[ i / s ];
-//        cout << ", P[" << (int)(bitseq & mask) << "]: " << (int)P[ (bitseq & mask) ] << endl;
-
-        return L[ i / l ] + S[ i / s ] + P[ (bitseq & mask) ];
+        return L[ i / l ] + S[ i / s ] + P[ (int)(bitseq & mask) ];
     };
 
     int select(int i){
@@ -136,22 +150,21 @@ public:
         while(s != e){
             m = (s+e)/2;
             r = rank(m+1);
-//            printf("i:%d,r:%d,s:%d,e:%d\n", i, r, s, e);
             if( i < r ){
                 e = m;
             }else{
                 s = m + 1;
             }
         }
-//        printf("i:%d,r:%d,s:%d,e:%d\n", i, r, s, e);
+
         return s;
     }
 
-    sBlock set(int i, char b){
+    block_s set(int i, char b){
         if( b - '0' > 0 ){
-            return B[ i >> 3 ] |= 1 << ( i & 0x07 );
+            return B[ DIVIDE8(i) ] |= 1 << MOD8(i);
         }else{
-            return B[ i >> 3 ] &= ~( 1 << ( i & 0x07 ) );
+            return B[ DIVIDE8(i) ] &= ~( 1 << MOD8(i) );
         }
     };
 };
@@ -170,9 +183,10 @@ int main(){
         if( bv.access(i) != B[i] ){
             printf("bv[%d]=%c, B[%d]=%c\n", i, bv.access(i), i, B[i]);
             result = false;
-        };
+        }
     }
     if(result){ printf("Access(i) test: clear.\n"); }
+    else{exit(1);}
 
     // a test for rank
     result = true;
@@ -186,6 +200,7 @@ int main(){
         }
     }
     if(result){ printf("Rank(i) test: clear.\n"); }
+    else{exit(1);}
 
     // a test for select
     result = true;
@@ -199,6 +214,7 @@ int main(){
         }
     }
     if(result){ printf("Select(i) test: clear.\n"); }
+    else{exit(1);}
 
     // a test for set
     result = true;
@@ -212,4 +228,5 @@ int main(){
         };
     }
     if(result){ printf("Set(i,b) test: clear.\n"); }
+    else{exit(1);}
 }
